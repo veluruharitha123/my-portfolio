8ccf56468f20d24b4f1ff35946b73ecf6e3e4cbc
veluruharitha123 <velurunagaharitha@gmail.com>
2026-03-31 18:01:48 +0530

    first commit

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    print("New Message:", name, email, message)
+
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
